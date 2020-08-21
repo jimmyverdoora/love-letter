d22940d4a472af551ad3cf4b8b982254b04c1dd3
mroselliz <m.roselli@meeters.org>
2020-08-21 17:14:09 +0200

    Fake game for testing removed

---
+++ game.js
@@ -0,0 +1,29 @@
+const { v4: uuidv4 } = require('uuid');
+const generateDeck = require('./deck');
+
+class GameManager {
+
+    createNewGame(nPlayers) {
+        const gameId = uuidv4();
+        const deck = generateDeck();
+        return {
+            id: gameId,
+            deck,
+            state: 'open',
+            players: [],
+            nPlayers,
+            discardedCard: null;
+        }
+    }
+
+    createPlayer(id, name) {
+        return {
+            id,
+            name,
+            hand: [],
+            pile: [],
+            points: 0
+        }
+    }
+
+}
